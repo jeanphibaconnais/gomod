ba1ee1bc41e3b4adda3a44b3cc8604313dc9b521
Hip <jean-philippe.baconnais@pole-emploi.fr>
2018-11-07 15:23:05 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gomod
+##### Project test of migration on Go 1.11
+
+This project allow to manipulate new go concept : modules
+
---
+++ main/putToUpperCase.go
@@ -0,0 +1,11 @@
+package main
+
+import "strings"
+
+func main() {
+
+}
+
+func PutToUppercase (s string)  string {
+	return strings.ToUpper(s)
+}
